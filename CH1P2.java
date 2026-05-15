8fb7683bfc000921d13c84a6e543386bf30c92ba
Ahsan <ahsan.moh426@gmail.com>
2026-05-15 16:34:23 -0400

    Chapter 1 + Chapter 2 first week of class

---
+++ CH1P2.java
@@ -0,0 +1,9 @@
+public class CH1P2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
